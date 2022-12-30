b871d1ca23ca58fe09207ac69d6d3e8f10b2e42c
Ganit kumar <104363798+ganitkumar517@users.noreply.github.com>
2022-12-30 10:53:17 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,48 @@
+
+import { useEffect, useState, useRef } from "react";
+import "./App.css";
+import Input from "./Components/Input/Input";
+import ShowTickets from "./Components/ShowTickets/ShowTickets";
+import Train from "./Components/Train";
+
+function App() {
+  const [showTrain, setShowTrain] = useState(true);
+  const [showInput, setShowInput] = useState(true);
+  const [showTickets, setShowTickets] = useState(false);
+  const [input, setInput] = useState(0);  
+  const [noOfBookedSeats, setNoOfBookedSeats] = useState(0);     
+
+  const bookedSeats = useRef([]);        
+  const seatsLeftInRow = useRef([7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 3]);
+
+  useEffect(() => {
+    setNoOfBookedSeats(noOfBookedSeats + input);
+  }, [input]);
+
+  return (
+    <div className="App">
+      {showTrain && <Train />}
+      {showInput && (
+        <Input
+          setInput={setInput}
+          setShowInput={setShowInput}
+          setShowTrain={setShowTrain}
+          setShowTickets={setShowTickets}
+          noOfBookedSeats={noOfBookedSeats}
+        />
+      )}
+      {showTickets && (
+        <ShowTickets
+          seatsLeftInRow={seatsLeftInRow}
+          bookedSeats={bookedSeats}
+          input={input}
+          setShowInput={setShowInput}
+          setShowTrain={setShowTrain}
+          setShowTickets={setShowTickets}
+        />
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Train.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Train() {
+  return (
+    <img src="https://media.istockphoto.com/id/1181249621/photo/electric-passenger-train-drives-at-high-speed-among-urban-landscape.jpg?s=612x612&w=0&k=20&c=020h1bnDZ4XNAd0V5tO5h21UyaryPryq8wlTwlBRwKc="></img>
+  )
+}
